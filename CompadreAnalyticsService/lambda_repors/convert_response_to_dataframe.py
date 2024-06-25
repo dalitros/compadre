d7848090c4034957c18f8fa06ef46bf7f84ce149
dalitros <67190869+dalitros@users.noreply.github.com>
2024-06-25 19:19:44 +0300

    Update convert_response_to_dataframe.py

--- CompadreAnalyticsService/lambda_repors/convert_response_to_dataframe.py
+++ CompadreAnalyticsService/lambda_repors/convert_response_to_dataframe.py
@@ -1,11 +1,3 @@
-import boto3
-# import pandas as pd
-
-s3 = boto3.client('s3')
-
-
-# from save_report import save_csv
-
 def convert_response_to_dataframe(response):
     # Extract the 'Items' from the response
     items = response['Items']
@@ -19,6 +11,5 @@ def convert_response_to_dataframe(response):
         data_dict = {key: val['N'] if 'N' in val else val['S'] for key, val in item.items()}
         # Append the dictionary to the list
         data_dicts.append(data_dict)
-
 
     return data_dicts
